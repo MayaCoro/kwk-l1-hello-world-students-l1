707ebc497ee172e652d776bd1a930e6105fa345d
MayaCoro <mayascoro@gmail.com>
2018-06-04 23:02:10 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,12 +1,3 @@
-def about_me 
-  puts "My name is Maya Coro"
-  puts "I grew up in Phoenix"
-  puts "I'm 18 years old"
-  puts "My favorite food is French toast"
-end
-5.times do
-about_me
-end
 
 puts "How are you feeling?"
 answer=gets.chomp
